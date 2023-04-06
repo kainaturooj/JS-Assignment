7f9b5694ae38df5c016eee7d2271e0f02431847b
kainaturooj <128933130+kainaturooj@users.noreply.github.com>
2023-04-07 04:14:09 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,104 @@
+// Assignment////
+
+//Q1..
+// let userNum1 = +prompt("Enter first number ");
+// let userNum2 = +prompt("Enter second number");
+// if (userNum1 > userNum2){
+//     console.log(userNum1)
+// }else{
+//     console.log(userNum2);
+// }
+
+
+
+//Q2...
+// let userNumSign = +prompt("Enter the number ");
+// if(userNumSign > 0){
+//     console.log("The sign is +");
+// }else{
+//     console.log("The sign is -");
+// }
+
+
+
+//Q3..
+// let userNum1 = +prompt("Enter first number ");
+// let userNum2 = +prompt("Enter second number");
+//  let userNum3 = +prompt("Enter third number");
+//  let userNum4 = +prompt("Enter fourth number");
+//  let userNum5 = +prompt("Enter fifth number");
+// if(userNum1 > userNum2 && userNum3 && userNum4 && userNum5 ){
+//     console.log(userNum1);
+// }
+// else if(userNum2 > userNum5 && userNum4 && userNum3 && userNum1 ){
+//     console.log(userNum2);
+// }
+//  else if(userNum3 > userNum5 && userNum4 && userNum2 && userNum1){
+//    console.log(userNum3);
+//  }
+//  else if(userNum4 > userNum5 && userNum3 && userNum2  && userNum1){
+//     console.log(userNum4)
+//  }
+//  else{
+//      console.log(userNum5);
+//  }
+
+
+
+//Q4...
+// for(let i=1; i<=15; i++){
+//     if(i % 2 === 0){
+//         console.log(i + " even");
+//     }else{
+//         console.log(i+" odd");
+//     }
+
+// }
+
+
+
+//Q5..
+// let userpercentage = +prompt("Enter your percentage");
+// if(userpercentage < 60){
+//     console.log("your grade if F");
+// }
+// else if(userpercentage < 70){
+//     console.log("your grade is D");
+// }
+// else if(userpercentage < 80){
+//     console.log("your grade is C");
+// }
+// else if(userpercentage < 90){
+//     console.log("your grade is B")
+// }
+// else if(userpercentage < 100){
+//     console.log("your grade is A");
+// }
+
+
+
+//Q6...
+// for(let i=1; i<=100; i++){
+//     if(i % 3 === 0 && i % 5 === 0){
+//         console.log(i + " FIZZBUZZ");
+//     }
+//     else if(i % 3 === 0){
+//         console.log(i + " FIZZ");
+//     }
+//     else if(i % 5 === 0){
+//         console.log(i + " BUZZ");
+//     }
+   
+// }
+
+
+
+//Q7...
+// for(let i=1; i<=5; i++){
+//     for(let j=1; j<=i; j++){
+//         document.write("*");
+//     }
+//     document.write("<br>");
+// }
+
+
